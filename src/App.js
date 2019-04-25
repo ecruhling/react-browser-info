c553326c5b65ff51af0cc9c0ae7d581724bcf7da
erik <erik@resourceatlanta.com>
2019-04-25 11:06:19 -0400

    import WhichBrowser library

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+///*global request*/
 import React from 'react';
 import './App.css';
-import request from 'which-browser';
-
 const WhichBrowser = require('which-browser');
-const result = new WhichBrowser(request.headers);
-
-console.log(result);
+const result = new WhichBrowser(navigator.userAgent);
 
 function App()
 {
+
   return (
       <div className="App">
-
         <div className="App-content">
-
+          <div className="App-browser">
+            You are using {result.browser.name} {result.browser.version.value}
+          </div>
+          <div className="App-os">
+            running on {result.os.toString()} to access this page
+          </div>
         </div>
-
       </div>
   );
 }
