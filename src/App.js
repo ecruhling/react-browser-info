e576c31157ce088964f34dcd958170a4c6f85bc5
erik <erik@resourceatlanta.com>
2019-04-25 12:21:45 -0400

    add logo / styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-///*global request*/
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
+import logo from './logo-resource-w.svg';
+
 const WhichBrowser = require('which-browser');
 const result = new WhichBrowser(navigator.userAgent);
 
@@ -8,12 +9,14 @@ function App()
 {
 
   return (
-      <div className="App">
-        <div className="App-content">
-          <div className="App-browser">
+      <div className={styles.App}>
+        <div className={styles.content}>
+          <a className={styles.logoLink} href={"https://resourceatlanta.com"}><img className={styles.logo} src={logo}
+                  alt="Resource Branding"/></a>
+          <div className={styles.browser}>
             You are using {result.browser.name} {result.browser.version.value}
           </div>
-          <div className="App-os">
+          <div className={styles.os}>
             running on {result.os.toString()} to access this page
           </div>
         </div>
