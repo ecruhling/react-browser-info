98ca35c7ad96b116c090105a352ccf9343310dfe
ecruhling <erik@resourceatlanta.com>
2024-02-16 13:57:28 -0600

    update react; fix some toString() errors; change logo

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// config-overrides.js
+module.exports = function override(config, env) {
+    // New config, e.g. config.plugins.push...
+    // console.log(JSON.stringify(config.resolve.fallback))
+    config.resolve.fallback = {
+        crypto: false,
+        ...config.resolve.fallback
+    };
+    return config
+}
--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
 import React from 'react';
-import styles from './App.module.css';
+import './App.css';
 import logo from './logo-resource-w.svg';
 
 const WhichBrowser = require('which-browser');
 const result = new WhichBrowser(navigator.userAgent);
 
 class ShowWindowDimensions extends React.Component {
-    state = { width: window.innerWidth, height: window.innerHeight };
+    state = {width: window.innerWidth, height: window.innerHeight};
+
     render() {
         return <span>{result.browser.name}'s viewport is {this.state.width}px width x {this.state.height}px height</span>;
     }
+
     updateDimensions = () => {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
+        this.setState({width: window.innerWidth, height: window.innerHeight});
     };
+
     componentDidMount() {
         window.addEventListener('resize', this.updateDimensions);
     }
+
     componentWillUnmount() {
         window.removeEventListener('resize', this.updateDimensions);
     }
 }
 
 function App() {
-
     return (
-        <div className={styles.App}>
-            <div className={styles.content}>
-                <a className={styles.logoLink} href={"https://resourceatlanta.com"}><img className={styles.logo}
-                                                                                         src={logo}
-                                                                                         alt="Resource Branding"/></a>
-                <div className={styles.browser}>
+        <div className="App">
+            <div className="content">
+                <a className="logoLink" href={"https://resourceatlanta.com"}>
+                    <img src={logo} className="logo" alt="Resource Branding"/>
+                </a>
+                <div className="browser">
                     You are using {result.browser.name} version {result.browser.version.value}
                 </div>
-                <div className={styles.os}>
-                    on
-                    your {result.device.manufacturer.toString()} {result.device.model.toString()} {result.device.type.toString()}
+                <div className="os">on your {result.device.manufacturer} {result.device.model} {result.device.type}
                 </div>
-                <div className={styles.os}>
-                    running {result.os.toString()}.
+                <div className="os">
+                    running {result.os.name}.
                 </div>
-                <div className={styles.os} style={{marginTop: '1rem'}}>
+                <div className="os" style={{marginTop: '1rem'}}>
                     <ShowWindowDimensions/>
                 </div>
             </div>
